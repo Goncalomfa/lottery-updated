63f60d98b9120dcf7e4726b51a26b59d1d92bd94
Goncalomfa <goncalomiguelalmeida@gmail.com>
2022-01-27 21:59:49 +0000

    Lottery contract, 0.8.9

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'arrive drama armed universe claim apart undo afraid network hawk urban secret',
+  'https://rinkeby.infura.io/v3/4b74aa0f5f684896af3bac08981ab69d'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
+//for lotteryreact it would be necessary to update the address and the abi of 'lottery.js'
